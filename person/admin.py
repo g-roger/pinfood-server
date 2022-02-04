6c3907d7c1c3e6f5dba3f8e93c6cbe53b718e442
Gabriel Roger <gabriel.nroger@gmail.com>
2022-02-03 22:49:25 -0300

    Add person admin

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from person.models import Person
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    def get_address_resume(self, obj):
+        return obj.address.state + ' - ' + str(obj.address.zip_code)
+
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_owner', 'get_address_resume')
+    ordering = ('-created_at',)
+    search_fields = ['name', 'email', 'last_name']
+    actions = ['export_as_csv']
+    list_filter = (
+        'is_active',
+        'is_owner'
+    )
+
+
+admin.site.register(Person, PersonAdmin)
